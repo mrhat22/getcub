ad6863b9eaef89c214a1e5df6dadf0135714102b
Mr hat <00001092919569sp@al.educacao.sp.gov.br>
2024-12-05 12:33:48 -0300

    aoooooooooo potenciaaaa

---
+++ pergutas.js
@@ -0,0 +1,29 @@
+criarCartao(
+    'progamaçao',
+    'oque é java sckcip ⌘',
+    'é uma linguagem'
+)
+
+criarCartao(
+    'progamaçao',
+    'voce sabe o maior erro de batalhar contra um urso?',
+    'usar o golpe mata leão'
+)
+
+criarCartao(
+    'progamaçao',
+    'oque é ʕ•́ᴥ•̀ʔ ⌘',
+    'é um DL^ð~æðDPçmdæł~'
+)
+
+criarCartao(
+    'progamaçao',
+    'oque é ʕ•́ᴥ•̀ʔ ⌘',
+    'é um DL^ð~æðDPçmdæł~'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'Como se diz hola en ingles',
+    'Oi em ingles é hai (rail)'
+)
